e4b8a6d6c987dca70e7f99efe55f49f54a67e3d3
c <yuvraj.m@yavar.in>
2023-08-14 09:44:18 +0000

    first commit and added raw data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='testing',
+    author='c17hawke',
+    license='MIT',
+)
